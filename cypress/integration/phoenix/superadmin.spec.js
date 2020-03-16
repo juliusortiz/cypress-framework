6eabeec7a3cfd53236bf7a289d5b19e3df9947d5
Jay <dice205test@gmail.com>
2020-03-16 15:44:28 +0800

    Initial commit

---
+++ cypress/integration/phoenix/superadmin.spec.js
@@ -0,0 +1,19 @@
+describe('Visit Phoenix Prod website', () => {
+    it('Visit Phoenix Website', function() {
+      cy.visit('https://qasphoenixdeliverysystem.phoenixfuels.ph/')
+      cy.get('.login100-form-btn').should('be.visible')
+      .screenshot()
+    })
+    it('Login Admin user', function() {
+      cy.get('.validate-inpu > .input100').select('Phoenix Petroleum')
+      cy.get('[data-validate="Enter username"] > .input100')
+      .type('pp.sa')
+      cy.get('.m-b-50 > .input100')
+      .type('P@ssw0rd123')
+      cy.get('.login100-form-btn')
+      .click()
+      .screenshot()
+    })
+
+
+})
