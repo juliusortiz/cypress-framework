f5d4b709d96b1e02ca311c0c9478c5d4ab179c84
Jay <dice205test@gmail.com>
2020-05-26 16:13:00 +0800

    Updated elements

--- cypress/integration/phoenix/visualRegressionTesting/dispatcherUI.spec.js
+++ cypress/integration/phoenix/visualRegressionTesting/dispatcherUI.spec.js
@@ -1,86 +1,70 @@
 describe('Dispatcher visual regression testing', () => {
-    it('Login page', function () {
+
+    before(() => {
         cy.navigate()
+        cy.login({ username: 'jaydispatcher', password: 'P@ssw0rd123'  })
+        cy.headerElements()
+    })
+
+    after(() => {
+         cy.notification()
+         cy.settings()
+         cy.logout()
+    })
+
+    beforeEach(() => {
+        cy.log('Start testing of page elements . . .')
+    })
+
+    afterEach(() => {
+        cy.log('End of testing of page elements . . . ')
     })
 
     it('Dashboard page', function () {
-        cy.login({ username: 'jaydispatcher', password: 'P@ssw0rd123' })
-        cy.headerElements()
-        cy.notification()
-        cy.get('.navbar__sidebar-toggle-icon').should('be.visible').click()
-        cy.get('[href="/dashboard/home"] > .sidebar-menu-item').should('be.visible').and('contain', 'Dashboard').click()
         cy.get('.menu-label-breed').should('be.visible').and('contain', 'Dashboard')
         cy.get('.container__select--active').should('be.visible')
         cy.get('.sales-input-container').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(1)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(2)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(3)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(4)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(5)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(6)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(7)').should('be.visible')
-        cy.get(':nth-child(1) > :nth-child(7) > #del_instruction').should('be.visible').and('contain', 'View').click()
-        cy.get('.modal-title').should('be.visible').and('contain', 'Delivery Instructions')
-        cy.get(':nth-child(3) > .font-weight-bold').should('be.visible').and('contain', 'Delivery Date')
-        cy.get('.pb-4 > .font-weight-bold').should('be.visible').and('contain', 'Delivery Time')
-        cy.get(':nth-child(5) > .d-flex').should('be.visible')
-        cy.get('.close > span').should('be.visible').click()
+
+        cy.get('.data-list-table__head > tr > :nth-child(1)').and('be.have.text', 'Trip Ticket No.')
+        cy.get('.data-list-table__head > tr > :nth-child(2)').and('be.have.text', 'Sold To')
+        cy.get('.data-list-table__head > tr > :nth-child(3)').and('be.have.text', 'SO No.')
+        cy.get('.data-list-table__head > tr > :nth-child(4)').and('be.have.text', 'OB No.')
+        cy.get('.data-list-table__head > tr > :nth-child(5)').and('be.have.text', 'Driver')
+        cy.get('.data-list-table__head > tr > :nth-child(6)').and('be.have.text', 'Plate No.')
+        cy.get('.data-list-table__head > tr > :nth-child(7)').and('be.have.text', 'Status')
+        cy.get('.data-list-table__head > tr > :nth-child(8)').and('be.have.text', 'Delivery Instructions')
     })
 
     it('Delivery history page', function () {
         cy.get('.navbar__sidebar-toggle-icon').click()
-        cy.get('[href="/dashboard/deliveries-history"] > .sidebar-menu-item').should('be.visible').and('contain', 'Deliveries History').click()
+        cy.get('[href="/dashboard/deliveries-history"] > .sidebar-menu-item').and('be.have.text', 'Deliveries History').click()
 
         cy.get('.menu-label-breed').should('be.visible').and('contain', 'Deliveries History')
         cy.get('.history-search__container').should('be.visible')
 
-        cy.get(':nth-child(1) > .history-card__container > .history-card__header > .icomoon').should('be.visible')
-        cy.get(':nth-child(1) > .history-card__container > .history-card__header > .history-card__header-text').should('be.visible')
-        cy.get(':nth-child(1) > .history-card__container > .history-card__info > .history-card__info-table > .history-card__info-body > :nth-child(1) > .history-card__info-header').should('be.visible')
-        cy.get(':nth-child(1) > .history-card__container > .history-card__info > .history-card__info-table > .history-card__info-body > :nth-child(2) > .history-card__info-header').should('be.visible')
-        cy.get(':nth-child(1) > .history-card__container > .history-card__footer > .btn').should('be.visible').click()
-
-        cy.get('.data-list-table__head > tr > :nth-child(1)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(2)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(3)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(4)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(5)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(6)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(7)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(8)').should('be.visible')
-
-        cy.get('.btn > .fas').should('be.visible').click()
     })
 
     it('Problems Reported page', function () {
         cy.get('.navbar__sidebar-toggle-icon').click()
-        cy.get('[href="/dashboard/problems-reported"] > .sidebar-menu-item').should('be.visible').and('contain', 'Problems Reported').click()
+        cy.get('[href="/dashboard/problems-reported"] > .sidebar-menu-item').and('be.have.text', 'Problems Reported').click()
 
-        cy.get('.data-list-table__head > tr > :nth-child(1)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(2)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(3)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(4)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(5)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(6)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(7)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(8)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(9)').should('be.visible')
-        cy.get('.data-list-table__head > tr > :nth-child(10)').should('be.visible')
+        cy.get('.data-list-table__head > tr > :nth-child(1)').and('be.have.text', 'Driver\'s Name')
+        cy.get('.data-list-table__head > tr > :nth-child(2)').and('be.have.text', 'Vehicle Code')
+        cy.get('.data-list-table__head > tr > :nth-child(3)').and('be.have.text', 'Plate Number')
+        cy.get('.data-list-table__head > tr > :nth-child(4)').and('be.have.text', 'Location')
+        cy.get('.data-list-table__head > tr > :nth-child(5)').and('be.have.text', 'Shipment No.')
+        cy.get('.data-list-table__head > tr > :nth-child(6)').and('be.have.text', 'Trip Ticket No.')
+        cy.get('.data-list-table__head > tr > :nth-child(7)').and('be.have.text', 'Outbound Delivery No.')
+        cy.get('.data-list-table__head > tr > :nth-child(8)').and('be.have.text', 'Date and Time')
+        cy.get('.data-list-table__head > tr > :nth-child(9)').and('be.have.text', 'Reason')
+        cy.get('.data-list-table__head > tr > :nth-child(10)').and('be.have.text', 'Problem Description')
     })
 
     it('Reasons page', function () {
         cy.get('.navbar__sidebar-toggle-icon').click()
-        cy.get('[href="/dashboard/manage-reasons-dispatcher"] > .sidebar-menu-item').should('be.visible').and('contain', 'Reasons').click()
-
-        cy.get('.menu-label-breed').should('be.visible').and('contain', 'Reasons')
+        cy.get('[href="/dashboard/manage-reasons-dispatcher"] > .sidebar-menu-item').and('be.have.text', 'Reasons').click()
+        cy.get('.menu-label-breed').should('be.visible').and('be.have.text', 'Reasons')
         cy.get('.container__select--active').should('be.visible')
     })
 
-    it('Settings page', function () {
-        cy.settings()
-    })
-    
-    it('Logout', function () {
-        cy.logout()
-    })
 })
